8d4c7ef203e0422c2417d49aa9e37529210411de
John Erickson <ericksonjohn08@hotmail.com>
2017-08-12 08:58:57 -0400

    Add variables scores, roundScores, activePlayer, and dice

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,5 +6,11 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
+
+var scores, roundScores, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+dice = Math.floor(Math.random() * 10) + 1;
