44a5b011e0ff2f6cba08ea33ac1dab6c7b31bf24
John Erickson <ericksonjohn08@hotmail.com>
2017-08-12 11:03:42 -0400

    Made score and current score for both players set to 0.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,15 +14,17 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	var dice = Math.floor(Math.random() * 5) + 1;
 	var diceDom = document.querySelector('.dice');
 	
 	diceDom.style.display = 'block';
-	
 	diceDom.src = '/public/images/dice-' + dice + '.png';
 	
 	if(dice !== 1) {
